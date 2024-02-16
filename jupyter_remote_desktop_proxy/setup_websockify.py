c7ab486ecdd4165b75b0ada97be05eb12e6f9d4d
Dylan Simon <dylan@dylex.net>
2024-02-16 17:37:16 -0500

    work with Tiger Xvnc directly
    TigerVNC 1.13 vncserver stopped accepting arguments, and instead only
    uses configuration files.  However, it's just a perl script that sets up
    the xinit command, so take over its functionality to run Xvnc directly.

--- jupyter_remote_desktop_proxy/setup_websockify.py
+++ jupyter_remote_desktop_proxy/setup_websockify.py
@@ -1,50 +1,74 @@
 import os
-import shlex
+import socket
 import tempfile
+import subprocess
 from shutil import which
 
-HERE = os.path.dirname(os.path.abspath(__file__))
-
-
 def setup_websockify():
-    # make a secure temporary directory for sockets
-    # This is only readable, writeable & searchable by our uid
-    sockets_dir = tempfile.mkdtemp()
-    sockets_path = os.path.join(sockets_dir, 'vnc-socket')
-    vncserver = which('vncserver')
+    xauthority = os.getenv('XAUTHORITY', os.path.join(os.getenv('HOME'), '.Xauthority'))
 
-    if not vncserver:
-        raise RuntimeError(
-            "vncserver executable not found, please install a VNC server"
-        )
+    def websockify_command(unix_socket=None):
+        # make a secure temporary directory for sockets
+        # This is only readable, writeable & searchable by our uid
+        sockets_dir = tempfile.mkdtemp()
+        sockets_path = os.path.join(sockets_dir, 'vnc-socket')
 
-    vnc_args = [vncserver, '-rfbunixpath', sockets_path]
-    socket_args = ['--unix-target', sockets_path]
-
-    if not os.path.exists(os.path.expanduser('~/.vnc/xstartup')):
-        vnc_args.extend(['-xstartup', os.path.join(HERE, 'share/xstartup')])
-
-    vnc_command = vnc_args + [
-            '-verbose',
-            '-geometry',
-            '1680x1050',
-            '-SecurityTypes',
-            'None',
-            '-fg',
+        cmd = [
+                'websockify',
+                '-v',
+                '--heartbeat',
+                '30',
+                '--unix-target',
+                sockets_path
         ]
+        if unix_socket:
+            cmd.append('--unix-listen')
+            cmd.append(unix_socket)
+        else:
+            # XXX this is insecure
+            cmd.append('{port}')
+
+        # assume one per user and port is open
+        # TODO dynamically detect display somehow?
+        uid = os.getuid()
+        display = f":{uid}"
+        host = socket.gethostname()
+
+        cookie = subprocess.run(['mcookie'], capture_output=True, check=True).stdout
+        subprocess.run(['xauth', '-f', xauthority], input=
+            f"add {host}{display} . ".encode() + cookie +
+            f"add {host}/unix{display} . ".encode() + cookie,
+            check=True)
+
+        xvnc = which('Xvnc')
+        cmd.extend([
+                '--',
+                'xinit',
+                '/etc/X11/xinit/Xsession',
+                '--',
+                xvnc,
+                display,
+                '-auth',
+                xauthority,
+                '-geometry',
+                '1680x1050',
+                '-rfbport=-1',
+                '-rfbunixpath',
+                sockets_path,
+                '-SecurityTypes',
+                'None'
+            ])
+
+        return cmd
 
     return {
-        'command': [
-            'websockify',
-            '-v',
-            '--heartbeat',
-            '30',
-            '{port}',
-        ]
-        + socket_args
-        + ['--'] + vnc_command,
+        'command': websockify_command,
+        'environment': {
+            'XAUTHORITY': xauthority
+        },
+        'unix_socket': True,
         'timeout': 30,
-        'new_browser_window': True,
+        'new_browser_tab': True,
         # We want the launcher entry to point to /desktop/, not to /desktop-websockify/
         # /desktop/ is the user facing URL, while /desktop-websockify/ now *only* serves
         # websockets.
