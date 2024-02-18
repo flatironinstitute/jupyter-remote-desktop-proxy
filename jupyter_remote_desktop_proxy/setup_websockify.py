cf563a34f5ecdc117ff9956b2ef434aec78d38b3
Dylan Simon <dylan@dylex.net>
2024-02-17 23:57:29 -0500

    convert to use server-proxy websockify
    from https://github.com/jupyterhub/jupyter-server-proxy/pull/447

--- jupyter_remote_desktop_proxy/setup_websockify.py
+++ jupyter_remote_desktop_proxy/setup_websockify.py
@@ -8,26 +8,6 @@ def setup_websockify():
     xauthority = os.getenv('XAUTHORITY', os.path.join(os.getenv('HOME'), '.Xauthority'))
 
     def websockify_command(unix_socket=None):
-        # make a secure temporary directory for sockets
-        # This is only readable, writeable & searchable by our uid
-        sockets_dir = tempfile.mkdtemp()
-        sockets_path = os.path.join(sockets_dir, 'vnc-socket')
-
-        cmd = [
-                'websockify',
-                '-v',
-                '--heartbeat',
-                '30',
-                '--unix-target',
-                sockets_path
-        ]
-        if unix_socket:
-            cmd.append('--unix-listen')
-            cmd.append(unix_socket)
-        else:
-            # XXX this is insecure
-            cmd.append('{port}')
-
         # assume one per user and port is open
         # TODO dynamically detect display somehow?
         uid = os.getuid()
@@ -41,8 +21,7 @@ def setup_websockify():
             check=True)
 
         xvnc = which('Xvnc')
-        cmd.extend([
-                '--',
+        cmd = [
                 'xinit',
                 '/etc/X11/xinit/Xsession',
                 '--',
@@ -52,12 +31,14 @@ def setup_websockify():
                 xauthority,
                 '-geometry',
                 '1680x1050',
-                '-rfbport=-1',
-                '-rfbunixpath',
-                sockets_path,
                 '-SecurityTypes',
                 'None'
-            ])
+            ]
+
+        if unix_socket:
+            cmd.extend(['-rfbport=-1', '-rfbunixpath', unix_socket])
+        else:
+            cmd.append('-rfbport={port}')
 
         return cmd
 
@@ -66,8 +47,9 @@ def setup_websockify():
         'environment': {
             'XAUTHORITY': xauthority
         },
+        'websockify': True,
         'unix_socket': True,
-        'timeout': 30,
+        'timeout': 60,
         'new_browser_tab': True,
         # We want the launcher entry to point to /desktop/, not to /desktop-websockify/
         # /desktop/ is the user facing URL, while /desktop-websockify/ now *only* serves
