6f7d6f61cf6a0710b6d9935576bc9dad8f695ac6
Paul Murray <plmrry@gmail.com>
2024-03-19 16:06:12 -0400

    add remote resizing toggle

--- js/more-tools.js
+++ js/more-tools.js
@@ -2,7 +2,7 @@
 
 import "./more-tools.css";
 
-import KeyTable from "@novnc/novnc/core/input/keysym.js"
+// import KeyTable from "@novnc/novnc/core/input/keysym.js";
 
 export function setupMoreTools({ trigger, rfb }) {
   let container = null;
@@ -20,9 +20,13 @@ export function setupMoreTools({ trigger, rfb }) {
   });
 }
 
+// Should be: rfb: import('@novnc/novnc/core/rfb')['default'];
+
 /**
- *
- * @param {{ container: HTMLDivElement; rfb: any; }} args
+ * @param {{
+ *   container: HTMLDivElement;
+ *   rfb: any;
+ * }} args
  */
 function render({ container, rfb }) {
   container.setAttribute(`id`, `more-tools-menu`);
@@ -31,27 +35,44 @@ function render({ container, rfb }) {
     <li>
       <button id="toggle-fullscreen">Toggle fullscreen</button>
     </li>
+    <li>
+      <input type="checkbox" id="remote-resizing" />
+      <label for="remote-resizing">Remote resizing</label>
+    </li>
   </ul>
   `;
+
+  container
+    .querySelector(`#toggle-fullscreen`)
+    ?.addEventListener(`click`, () => {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+      } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    });
+
+  /** @type {HTMLInputElement | null} */
+  const resizing_checkbox = container.querySelector(`#remote-resizing`);
+
+  if (resizing_checkbox) resizing_checkbox.checked = rfb.resizeSession;
+
+  resizing_checkbox?.addEventListener(`change`, (event) => {
+    // @ts-ignore
+    const checked = event.currentTarget?.checked;
+    rfb.resizeSession = checked;
+  });
 
   // <li>
   //   <button id="toggle-windows">Toggle Windows key</button>
   // </li>
 
-  container.querySelector(`#toggle-fullscreen`)?.addEventListener(`click`, () => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen();
-    }
-  })
-
-  container.querySelector(`#toggle-windows`)?.addEventListener(`click`, () => {
-    // rfb.sendKey(KeyTable.XK_Super_L, "MetaLeft", true);
-    // rfb.sendKey(KeyTable.XK_Control_L, "ControlLeft", true);
-    // rfb.sendKey(KeyTable.XK_Alt_L, "AltLeft", true);
-    // rfb.sendKey(KeyTable.XK_Tab, "Tab");
-    // rfb.sendKey(KeyTable.XK_Escape, "Escape");
-    // rfb.sendKey(KeyTable.XK_a);
-  });
+  // container.querySelector(`#toggle-windows`)?.addEventListener(`click`, () => {
+  //   // rfb.sendKey(KeyTable.XK_Super_L, "MetaLeft", true);
+  //   // rfb.sendKey(KeyTable.XK_Control_L, "ControlLeft", true);
+  //   // rfb.sendKey(KeyTable.XK_Alt_L, "AltLeft", true);
+  //   // rfb.sendKey(KeyTable.XK_Tab, "Tab");
+  //   // rfb.sendKey(KeyTable.XK_Escape, "Escape");
+  //   // rfb.sendKey(KeyTable.XK_a);
+  // });
 }
